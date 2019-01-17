0729e458c05fa6186ed5343a6d15f0c406df268a
Michael Kalygin <michael.kalygin@gmail.com>
2019-01-18 00:37:36 +0300

    Remove response time measurements

--- src/server/main.js
+++ src/server/main.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const responseTime = require('response-time');
 const morgan = require('morgan');
 const router = require('./router');
 const helpers = require('./helpers');
@@ -21,9 +20,6 @@ if (config.isDevelopment) {
 app.set('views', config.views);
 app.set('view engine', 'pug');
 app.locals.basedir = config.views;
-
-// Measure response time of every request.
-app.use(responseTime((req, res, time) => console.log(`Time: ${time}`)));
 
 // Log every request.
 app.use(morgan('combined'));
