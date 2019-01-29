fec71a2d1e83efb599b4d823e31731ba14dc8d66
Michael Kalygin <michael.kalygin@gmail.com>
2019-01-30 00:15:24 +0300

    Refactor InfoCard with react transition group

--- src/scripts/components/InfoCard.js
+++ src/scripts/components/InfoCard.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { Portal } from 'react-portal';
+import { CSSTransition } from 'react-transition-group';
 import * as PhotoSwipe from 'photoswipe';
 import * as PhotoSwipeTheme from 'photoswipe/dist/photoswipe-ui-default';
 import tracker from '../utils/tracker';
@@ -9,18 +10,27 @@ import tracker from '../utils/tracker';
 class InfoCard extends Component {
   static propTypes = {
     marker: PropTypes.object,
-    visible: PropTypes.bool,
     onClose: PropTypes.func,
   };
 
   static defaultProps = {
-    visible: false,
     onClose: () => {},
   };
 
-  componentDidMount() {
+  contentRef = React.createRef();
+
+  scrollHintRef = React.createRef();
+
+  onEnter = () => {
+    this.content = this.contentRef.current;
+    this.scrollHint = this.scrollHintRef.current;
+
     this.content.addEventListener('scroll', this.onScroll);
-  }
+  };
+
+  onExit = () => {
+    this.content.removeEventListener('scroll', this.onScroll);
+  };
 
   componentDidUpdate() {
     const { marker } = this.props;
@@ -34,10 +44,6 @@ class InfoCard extends Component {
         this.hideScrollHint();
       }
     }
-  }
-
-  componentWillUnmount() {
-    this.content.removeEventListener('scroll', this.onScroll);
   }
 
   initGallery() {
@@ -92,8 +98,8 @@ class InfoCard extends Component {
   };
 
   render() {
-    const { marker, visible } = this.props;
-    const classes = classNames(['info-card-container', { visible }]);
+    const { marker } = this.props;
+    const classes = classNames(['info-card-container', { visible: !!marker }]);
     const portalNode = document.querySelector('.map-viewer-controls');
 
     const currentMarker = marker || {};
@@ -102,54 +108,56 @@ class InfoCard extends Component {
       : [];
 
     return (
-      <Portal node={portalNode}>
-        <div
-          className={classes}
-          onMouseDown={this.onStopPropagation}
-          onTouchStart={this.onStopPropagation}
-        >
-          <div className="info-card-overlay" onClick={this.onClose} />
+      <CSSTransition
+        in={!!marker}
+        timeout={200}
+        classNames="visible"
+        onEnter={this.onEnter}
+        onExit={this.onExit}
+        unmountOnExit
+      >
+        <Portal node={portalNode}>
+          <div
+            className={classes}
+            onMouseDown={this.onStopPropagation}
+            onTouchStart={this.onStopPropagation}
+          >
+            <div className="info-card-overlay" onClick={this.onClose} />
 
-          <div className="info-card">
-            <div
-              className="content"
-              ref={node => {
-                this.content = node;
-              }}
-            >
-              <div className="title">{currentMarker.name}</div>
-              <div className="photos" onClick={() => this.initGallery()}>
-                <div className="photos-container">
-                  {photos.map(photo => (
-                    <img key={photo.src} src={photo.src} />
-                  ))}
+            <div className="info-card">
+              <div className="content" ref={this.contentRef}>
+                <div className="title">{currentMarker.name}</div>
+                <div className="photos" onClick={() => this.initGallery()}>
+                  <div className="photos-container">
+                    {photos.map(photo => (
+                      <img key={photo.src} src={photo.src} />
+                    ))}
+                  </div>
+                  <div className="photos-button">Смотреть фото</div>
                 </div>
-                <div className="photos-button">Смотреть фото</div>
+                <div
+                  className="description"
+                  dangerouslySetInnerHTML={{
+                    __html: currentMarker.description,
+                  }}
+                />
               </div>
+
+              <div className="close" onClick={this.onClose}>
+                <span className="icon-cancel" />
+              </div>
+
               <div
-                className="description"
-                dangerouslySetInnerHTML={{
-                  __html: currentMarker.description,
-                }}
-              />
-            </div>
-
-            <div className="close" onClick={this.onClose}>
-              <span className="icon-cancel" />
-            </div>
-
-            <div
-              className="scroll-hint"
-              onClick={this.scrollBottom}
-              ref={node => {
-                this.scrollHint = node;
-              }}
-            >
-              <span className="icon-left-open-big" />
+                className="scroll-hint"
+                onClick={this.scrollBottom}
+                ref={this.scrollHintRef}
+              >
+                <span className="icon-left-open-big" />
+              </div>
             </div>
           </div>
-        </div>
-      </Portal>
+        </Portal>
+      </CSSTransition>
     );
   }
 }
